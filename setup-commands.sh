7451545c4cb6b5f2c3efe31b1a3fbbea287aba93
andrewcathcart <andrew.cathcart@salecycle.com>
2019-06-03 23:48:39 +0100

    altered create_project bash file

--- setup-commands.sh
+++ setup-commands.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 function create_project() {
-    cd
     pipenv run python3 create_project.py $1
-    cd /Users/andrew.cathcart/dev/python/
+    cd /Users/andrew.cathcart/dev/python/$1
     git init
     git remote add origin git@github.com:AndrewCathcart/$1.git
     touch README.md
