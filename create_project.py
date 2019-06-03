289643a870d39205e33fca6939915706c8722345
andrewcathcart <andrew.cathcart@salecycle.com>
2019-06-03 23:40:37 +0100

    using token generated by github now

--- create_project.py
+++ create_project.py
@@ -1,23 +1,27 @@
 import sys
 import os
 from github import Github
+from secrets import PERSONAL_ACCESS_TOKEN
 
 # path of the project folder you'd like to create the project in.
 path = "/Users/andrew.cathcart/dev/python/"
 
+# specify username or personal access token generated at https://github.com/settings/tokens
+username = ""
+password = ""
+token = PERSONAL_ACCESS_TOKEN
+
 
 def create_project():
     ''' Creates a project folder at the specified path & sets up a repo on your Github account. '''
-    # TODO error handling
     project_name = str(sys.argv[1])
-    username = sys.argv[2]
-    password = sys.argv[3]
-
     full_project_path = path + project_name
-    user = Github(username, password)
 
+    # g = Github(username, password)
+    g = Github(token)
     os.makedirs(full_project_path)
-    repo = user.create_repo(project_name)
+
+    repo = g.get_user().create_repo(project_name)
 
     print("Successfully created a project at", full_project_path)
     print("Successfully created a repository named",
--- setup-commands.sh
+++ setup-commands.sh
@@ -2,10 +2,10 @@
 
 function create_project() {
     cd
-    pipenv run python3 create_project.py $1 $2 $3
+    pipenv run python3 create_project.py $1
     cd /Users/andrew.cathcart/dev/python/
     git init
-    git remote add origin git@github.com:$2/$1.git
+    git remote add origin git@github.com:AndrewCathcart/$1.git
     touch README.md
     git add .
     git commit -m "Initial commit"
